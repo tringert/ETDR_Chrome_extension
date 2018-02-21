0dfc324c99dc6eaeb3ad08ecc90058a5788a2a79
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2018-02-21 21:17:28 +0100

    -

---
+++ background.js
@@ -0,0 +1,23 @@
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?etdr\.gov\.hu\/(RDProcessAction\/ProcessActionEdit|RDProcessByUser\/ProcessEdit|ProcessByOffice\/ProcessEdit|ProcessAction\/ProcessActionEdit)/;
+
+// A function to use as callback
+function doStuffWithDom(infos) {
+
+	for (i = 0; i < infos.length; i++) { 
+		
+		downloading = chrome.downloads.download({
+			url : infos[i].link,
+			filename : infos[i].filename,
+			conflictAction : 'uniquify'
+		});
+	}
+}
+
+// When the browser-action button is clicked...
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // ...check the URL of the active tab against our pattern and...
+    if (urlRegex.test(tab.url)) {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+    }
+});
---
+++ content.js
@@ -0,0 +1,40 @@
+// Listen for messages
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    // If the received message has the expected format...
+    if (msg.text === 'report_back') {
+        // ... call the specified callback, passing
+        // the data back to the background page
+        sendResponse(getDownloadInfo());
+    }
+});
+
+// Get the info for download
+function getDownloadInfo(){
+	
+    var links = document.querySelectorAll('#uploadedDocs_DXMainTable a');
+    var infos = [];
+
+    for (link of links) {
+        let href = link.getAttribute('href');
+        if (href.match(/\/Document\/Download\//)) {
+			infos.push({ link: `https://www.etdr.gov.hu${href}` });
+		}
+    }
+
+    var text = document.querySelectorAll('#uploadedDocs_DXMainTable tr');
+    // Filename position in table
+    var fpos = 1;
+    if (window.location.href.match(/etdr\.gov\.hu\/RDProcessByUser\/ProcessEdit/) ||
+        window.location.href.match(/etdr\.gov\.hu\/ProcessByOffice\/ProcessEdit/) ||
+        window.location.href.match(/etdr\.gov\.hu\/ProcessAction\/ProcessActionEdit/)) {
+        fpos = 3;
+    }
+
+    for (i = 0; i < infos.length; i++) {
+
+        let item = text[i + 1].cells[fpos].innerHTML.replace(/<.*\/a>/, '').replace(/\s+$/, '');
+        infos[i].filename = item;
+    }
+
+    return infos;
+}
