67368cdb7e12f097cbf911e0e9e291d6df766057
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2019-11-30 16:27:43 +0100

    Download to a folder named after the process number

--- background.js
+++ background.js
@@ -1,23 +1,41 @@
 var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?etdr\.gov\.hu\/(RDProcessAction\/ProcessActionEdit|RDProcessByUser\/ProcessEdit|ProcessByOffice\/ProcessEdit|ProcessAction\/ProcessActionEdit)/;
+var devEnvRegex = /^https?:\/\/(?:[^./?#]+\.)?fejl\.etdr\.gov\.hu\/(RDProcessAction\/ProcessActionEdit|RDProcessByUser\/ProcessEdit|ProcessByOffice\/ProcessEdit|ProcessAction\/ProcessActionEdit)/;
+var isDevEnv = new Boolean(false);
 
 // A function to use as callback
-function doStuffWithDom(infos) {
+function doStuffWithDom(jsonData) {
+    var infos = JSON.parse(jsonData);
 
-	for (i = 0; i < infos.length; i++) { 
-		
-		downloading = chrome.downloads.download({
-			url : infos[i].link,
-			filename : infos[i].filename,
-			conflictAction : 'uniquify'
-		});
-	}
+    // Set the folder name
+    var downloadPrefix = infos.processNumber === "" ? "" : infos.processNumber.replace("/", "_") + "/";
+
+    for (i = 0; i < infos.loc.length; i++) {
+
+        if (isDevEnv) {
+            infos.loc[i].link = infos.loc[i].link.replace('www.etdr.gov.hu', 'fejl.etdr.gov.hu');
+        }
+
+        downloading = chrome.downloads.download({
+            url: infos.loc[i].link,
+            filename: downloadPrefix + infos.loc[i].filename,
+            conflictAction: 'uniquify'
+        });
+    }
+
+    isDevEnv = false;
 }
 
 // When the browser-action button is clicked...
 chrome.browserAction.onClicked.addListener(function (tab) {
+
+    // When started in the development environment we need to change the download url
+    if (devEnvRegex.test(tab.url)) {
+        isDevEnv = true;
+    }
+
     // ...check the URL of the active tab against our pattern and...
     if (urlRegex.test(tab.url)) {
         // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+        chrome.tabs.sendMessage(tab.id, { text: 'report_back' }, doStuffWithDom);
     }
 });
