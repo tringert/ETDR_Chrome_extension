9cd0657cb1f18139bc72bdab29f9ce5407844d28
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2019-12-01 20:38:00 +0100

    Changelog window added: it will be displayed when this version installed or an update occur. A common download folder added When the process number couldn't be found, use datetime as the folder name

--- background.js
+++ background.js
@@ -12,13 +12,13 @@ function doStuffWithDom(jsonData) {
         ? downloadFolder + currentDateTimeAsFolderName()
         : downloadFolder + infos.processNumber.replace("/", "_") + "/";
 
-    for (i = 0; i < infos.loc.length; i++) {
+    for (var i = 0; i < infos.loc.length; i++) {
 
         if (isDevEnv) {
             infos.loc[i].link = infos.loc[i].link.replace('www.etdr.gov.hu', 'fejl.etdr.gov.hu');
         }
 
-        downloading = chrome.downloads.download({
+        var downloading = chrome.downloads.download({
             url: infos.loc[i].link,
             filename: downloadPrefix + infos.loc[i].filename,
             conflictAction: 'uniquify'
@@ -26,6 +26,61 @@ function doStuffWithDom(jsonData) {
     }
 
     isDevEnv = false;
+
+    // Get the local storage to determine if a new install or an update occured
+    var gettingItem = chrome.storage.local.get(["ETDR_ExtVersion", "ETDR_ShowChangeLog"], function (res) {
+        detectVersionChange(res.ETDR_ExtVersion, res.ETDR_ShowChangeLog);
+    });
+}
+
+// When a new install or an update occured, show a changelog page to the user
+function detectVersionChange(storedVersion, showChangelog) {
+    // Get the extensions actual version from the manifest file
+    var extVersion = chrome.runtime.getManifest().version.replace(/\./g, "");
+
+    // Check if the extension's version is stored in the local storage and if it is older than the current one. If yes, then store it and set to show the changelog.
+    if (storedVersion === undefined || storedVersion < extVersion) {
+        storeCurrentVersion(extVersion);
+        storeShowChangelog(true);
+        showChangelog = true;
+    }
+
+    // If the showing the changelog window isn't stored in the local storage, then store it and set it to show
+    if (showChangelog === undefined) {
+        storeShowChangelog(true);
+        showChangelog = true;
+    }
+
+    // Show the changelog if necessary and store that next time the changelog window won't be displayed
+    if (showChangelog) {
+        openChangelog();
+        storeShowChangelog(false);
+    }
+}
+
+// Changelog window show value store
+function storeShowChangelog(value) {
+    chrome.storage.local.set({
+        ETDR_ShowChangeLog: value
+    });
+}
+
+// Current extension's version store
+function storeCurrentVersion(value) {
+    chrome.storage.local.set({
+        ETDR_ExtVersion: value
+    });
+}
+
+function openChangelog() {
+    let page = {
+        type: "popup",
+        url: "backgroundpage.html",
+        width: 800,
+        height: 400
+    };
+
+    let creating = chrome.windows.create(page);
 }
 
 function currentDateTimeAsFolderName() {
